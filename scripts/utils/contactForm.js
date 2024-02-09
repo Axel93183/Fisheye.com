19613b2a938af8f73d11ed5875a62081d28048b9
Axel GMX <agmx183@gmail.com>
2024-02-09 13:59:57 +0100

    Modal de Contact : formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,18 +1,40 @@
-const contactButton = document.getElementById("contact_button");
-contactButton.setAttribute("aria-label", "Contactez-moi");
+async function getDatas() {
+  const response = await fetch("data/photographers.json");
+  const data = await response.json();
+  return data;
+}
+async function displayPhotographerName() {
+  const photographersData = await getDatas();
+  const filteredPhotographer = photographersData.photographers.filter(
+    (photographer) => photographer.id === photographerId
+  );
 
-function displayModal() {
-  const modal = document.getElementById("contact_modal");
-  modal.style.display = "block";
+  console.log("======");
+  console.log(filteredPhotographer);
+  console.log("======");
+  // const modalTitle = document.querySelector(".modal-title");
+  // modalTitle.textContent = filteredPhotographer.name;
 }
 
-contactButton.addEventListener("click", displayModal);
+const body = document.getElementById("body");
+const contactModal = document.getElementById("contact_modal");
+const form = document.forms.reserve;
+const contactButton = document.getElementById("contact_button");
+
+contactButton.setAttribute("aria-label", "Contactez-moi");
 
 const closeCross = document.getElementById("close-cross");
 
+function openModal() {
+  contactModal.style.display = "block";
+  displayPhotographerName();
+}
+
+contactButton.addEventListener("click", openModal);
+
 function closeModal() {
-  const modal = document.getElementById("contact_modal");
-  modal.style.display = "none";
+  contactModal.style.display = "none";
+  body.style.overflow = "initial";
 }
 
 closeCross.addEventListener("click", closeModal);
